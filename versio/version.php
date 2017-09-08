6f32b98a8f3aeae6aa164ca45a6bef91213aa1a9
“Reshad <reshad@versio.nl>
2017-09-08 12:52:25 +0200

    wefact

---
+++ versio/version.php
@@ -0,0 +1,22 @@
+<?php
+$version['name']            = "Versio";
+$version['api_version']     = "1.0 REST API";
+$version['date']            = "2017-08-02";
+$version['wefact_version']  = "4.1.0";
+$version['autorenew']       = true;
+$version['handle_support']  = true;
+$version['cancel_direct']   = true;
+$version['cancel_expire']   = true;
+
+$version['dev_logo']		= 'https://www.versio.nl/assets/images/logos/versio-logo.png';
+$version['dev_author']		= 'Versio B.V.';
+$version['dev_website']		= 'https://www.versio.nl/';
+$version['dev_email']		= 'support@versio.nl';
+
+$version['domain_support']  = true;
+$version['ssl_support']   	= true;
+
+$version['dns_management_support'] = true;
+$version['dns_templates_support'] = true;
+$version['dns_records_support'] = true;
+?>
